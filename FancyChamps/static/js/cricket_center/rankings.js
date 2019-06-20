32d839a2a6159c33a52e05b9754b5ee460996d0e
FightWithCode <rahulsharmanoj@gmail.com>
2019-06-20 08:41:17 +0000

    Froom Server

--- FancyChamps/static/js/cricket_center/rankings.js
+++ FancyChamps/static/js/cricket_center/rankings.js
@@ -42,13 +42,13 @@ $('.LeagueName').click(function(){
                 }
             }
             if(data.confirmed){
-                $('.PayoutDivContestTypes').append("<span class=\"ContestTypesJs\">C</span><span class=\"ContestTypeInfo\">This is a confirmed contest.</span><br>")    
+                $('.PayoutDivContestTypes').append("<span class=\"ContestTypesJs\">C</span><span class=\"ContestTypeInfo\">This is a confirmed contest.</span><br>")
             }
             if(data.bonus_contest){
-                $('.PayoutDivContestTypes').append("<span class=\"ContestTypesJs\">B</span><span class=\"ContestTypeInfo\">You can use bonus in this contest.</span><br>")  
+                $('.PayoutDivContestTypes').append("<span class=\"ContestTypesJs\">B</span><span class=\"ContestTypeInfo\">You can use bonus in this contest.</span><br>")
             }
             if(data.multiple_entry){
-                $('.PayoutDivContestTypes').append("<span class=\"ContestTypesJs\">M</span><span class=\"ContestTypeInfo\">You can join this contest with multiple teams.</span><br>")  
+                $('.PayoutDivContestTypes').append("<span class=\"ContestTypesJs\">M</span><span class=\"ContestTypeInfo\">You can join this contest with multiple teams.</span><br>")
             }
         }
     })
@@ -201,8 +201,9 @@ $('.RankInfoCard').click(function () {
                 }
 
                 if (data.total_allrounders == 1){
+                    // console.log(data["Player"+[data.total_batsmen+0+2]])
                     AllrounderDivWidth = 100/data.total_allrounders+"%"
-                    $(".AllrounderPreview").html("<div class=\"PlayerView\" style=\"width: " + AllrounderDivWidth + "\"><center><img class=\"player_image\" src=\"/static/images/allrounder.png\"></center><span class=\"PlayerNameView\">" + data["Player"+[data.total_batsmen+i+2]] + "<br>" + data["Player"+[data.total_batsmen+i+2]+"_Points"] + "</span></div>")
+                    $(".AllrounderPreview").html("<div class=\"PlayerView\" style=\"width: " + AllrounderDivWidth + "\"><center><img class=\"player_image\" src=\"/static/images/allrounder.png\"></center><span class=\"PlayerNameView\">" + data["Player"+[data.total_batsmen+0+2]] + "<br>" + data["Player"+[data.total_batsmen+0+2]+"_Points"] + "</span></div>")
                 }
 
                 if (data.total_allrounders == 2){
